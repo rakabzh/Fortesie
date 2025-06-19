ba30756b751dabbb771456065c6909ccd2f51f62
rakabzh <148957373+rakabzh@users.noreply.github.com>
2025-06-19 18:24:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 import axios from "axios";
 
-async function login(url) {
-  const params = new URLSearchParams();
-  params.append("client_id", "green-euro");
-  params.append("client_secret", "lGoY3XIZsnrmnG7iopgAKc5zzvsx2xHI");
-  params.append("grant_type", "client_credentials");
-  const reponse = await axios.post(url, params);
-  return reponse.data.access_token;
-}
-
 async function getData(url, token) {
   try {
     const params = {
@@ -31,11 +22,7 @@ async function getData(url, token) {
 async function main() {
   console.log(
     await getData(
-      "https://analytics.fortesie.epu.ntua.gr/calculate_overall_degree_days/get_sensor_data_by_id/",
-      await login(
-        "https://fortesie.eurodyn.com/auth/realms/fortesie/protocol/openid-connect/token"
-      )
-    )
+      "https://analytics.fortesie.epu.ntua.gr/calculate_overall_degree_days/get_sensor_data_by_id/")
   );
 }
 
